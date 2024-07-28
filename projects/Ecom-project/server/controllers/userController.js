472f4e77a8b0fe394f710e53e11394eef3515f3a
akashkhot12 <akashkhot03@gmail.com>
2024-07-28 15:39:15 +0530

    adding cookie

--- projects/Ecom-project/server/controllers/userController.js
+++ projects/Ecom-project/server/controllers/userController.js
@@ -1,44 +1,66 @@
 const Users = require("../models/userModel");
 const bcrypt = require("bcrypt");
-const jwt  = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const userController = {
-  register: async (req, res) => {
-    try {
-      // take creadentials from users.
-      const { name, email, password } = req.body;
+    register: async (req, res) => {
+        try {
+            // take creadentials from users.
+            const { name, email, password } = req.body;
 
-      // check email already exist or not.
-      const user = await Users.findOne({ email });
-      if (user)
-        return res.status(400).json({ msg: "Email Already Registered" });
+            // check email already exist or not.
+            const user = await Users.findOne({ email });
+            if (user)
+                return res.status(400).json({ msg: "Email Already Registered" });
 
-      // check password length is more than or equal to 6 digits.
-      if (password.length < 6)
-        return res
-          .status(400)
-          .json({ msg: "Password is atleast 6 character." });
+            // check password length is more than or equal to 6 digits.
+            if (password.length < 6)
+                return res
+                    .status(400)
+                    .json({ msg: "Password is atleast 6 character." });
 
-      // password bcypted
-      const passwordHash = await bcrypt.hash(password, 10);
+            // password bcypted
+            const passwordHash = await bcrypt.hash(password, 10);
 
-      // create new user for storing data into database.
-      const newUsers = new Users({
-        name,
-        email,
-        password: passwordHash,
-      });
+            // create new user for storing data into database.
+            const newUsers = new Users({
+                name,
+                email,
+                password: passwordHash,
+            });
 
-    //   save data to user table 
-      await newUsers.save();
+            // save data to user table
+            await newUsers.save();
 
-    //   create jwt token for user 
+            // create jwt token for user
+            const accessToken = createAccessToken({ id: newUsers._id });
 
-      res.json({ msg: `${name} Registered Success.` });
-    } catch (error) {
-      return res.status(500).json({ msg: error.message });
-    }
-  },
+            // refresh token
+            const refreshToken = createRefreshToken({ id: newUsers._id });
+
+            // adding cookie
+            res.cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                path: "/user/refresh_token",
+            });
+
+            res.json({ msg: accessToken });
+        } catch (error) {
+            return res.status(500).json({ msg: error.message });
+        }
+    },
+};
+
+const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRETE, {
+        expiresIn: "1d",
+    });
+};
+
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRETE, {
+        expiresIn: "7d",
+    });
 };
 
 module.exports = userController;
