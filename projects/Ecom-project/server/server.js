cc813a23cdc67e531361cf15c202839e0fd462a0
akashkhot12 <akashkhot03@gmail.com>
2024-07-28 13:53:19 +0530

    adding models

--- projects/Ecom-project/server/server.js
+++ projects/Ecom-project/server/server.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
+require('dotenv').config()
 
+
+app.get('/',(req,res)=>{
+    res.json({msg:"hello world"})
+})
 
 app.listen(PORT,()=>{
     console.log("server is running....");
 })
+
+
+// connected mongo db 
+
+const URI = process.env.MONGODB_URL;
+
+mongoose.connect(URI,{
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
