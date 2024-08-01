e86b5de245d27a77765a5594236100a0c65b06d2
akashkhot12 <akashkhot03@gmail.com>
2024-08-01 12:32:53 +0530

    creating crud operations

--- projects/Ecom-project/server/controllers/categoryController.js
+++ projects/Ecom-project/server/controllers/categoryController.js
@@ -32,8 +32,16 @@ const categoryController = {
   deleteCategory: async (req, res) => {
     try {
       await Category.findByIdAndDelete(req.params.id);
-
       return res.status(200).json({ msg: "Deleted a Category" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  updateCategory: async (req, res) => {
+    try {
+      const { name } = req.body;
+      await Category.findByIdAndUpdate({ _id: req.params.id }, { name });
+      res.json({ msg: "updated" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
--- projects/Ecom-project/server/routes/categoryRoutes.js
+++ projects/Ecom-project/server/routes/categoryRoutes.js
@@ -11,6 +11,7 @@ router
 
 router
   .route("/category/:id")
-  .delete(auth, authAdmin, categoryController.deleteCategory);
+  .delete(auth, authAdmin, categoryController.deleteCategory)
+  .put(auth, authAdmin, categoryController.updateCategory);
 
 module.exports = router;
